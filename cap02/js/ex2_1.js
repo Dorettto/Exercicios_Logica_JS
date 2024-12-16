312ff1d67298353415180cb374b5cc0842456ae0
Natália Doreto <dorettto@icloud.com>
2024-12-16 15:53:43 -0300

    Add files via upload

---
+++ cap02/js/ex2_1.js
@@ -0,0 +1,8 @@
+const frm = document.querySelector("form") // cria uma variável referente ao elemento de id=form
+const resp = document.querySelector("h3") // cria uma variável referente a todos os h3 do documento
+
+frm.addEventListener("submit", (e) =>{
+    const nome = frm.inNome.value // obtèm o que foi digitado no campo inNome e transforma na variável nome
+    resp.innerText = `Olá ${nome}` //exibe a resposta do programa
+    e.preventDefault() //evita o envio do form para que a resposta fique exibida na tela
+})
